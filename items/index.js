d03bfcdad713bd72918616b069ba47b61bcb0eb7
Simrit Dhillon <simritdhillon@Simrits-MacBook-Pro.local>
2020-12-02 20:48:53 -0500

    Add new item 1 img, delete old, edit line

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/FavouriteFoodBaskets750x500_xraj9x.png"},
+export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/Item1_img.png"},
 {"id": "item2", "fname":"item2.md", "specialImage":"/img/FavouriteDay750x500_pdni4p.png"},
 {"id": "item3", "fname":"item3.md", "specialImage":"/img/twiliobot750x500_z7anzp.png"},
 {"id": "item4", "fname":"item4.md", "specialImage":"https://res.cloudinary.com/salesucation-com-inc/image/upload/v1523215138/DroidfootGame750x500_bwphbe.png"},
