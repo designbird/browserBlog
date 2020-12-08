5eae0da79beae577c0bdee615709f0c186b31a90
Simrit Dhillon <simritdhillon@Simrits-MacBook-Pro.local>
2020-12-07 19:51:20 -0500

    adding img

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
 export default [{"title":"About", "fname":"about.md", "specialImage":"/img/menagerie.png"},
+{"title":"Header Image", "specialImage":"/img/backgroundheader,jpg"},
 {"title":"index", "fname":"index.md"},
 {"title":"Contact", "fname":"contact.md"}];
