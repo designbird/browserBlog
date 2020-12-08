afcd5386314dee4a454a2d8a7e42cc260305031f
Simrit Dhillon <simritdhillon@Simrits-MacBook-Pro.local>
2020-12-07 20:01:45 -0500

    trying to fix #2

--- items/index.js
+++ items/index.js
@@ -2,5 +2,4 @@ export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/Item1_i
 {"id": "item2", "fname":"item2.md", "specialImage":"/img/Item2_img.png"},
 {"id": "item3", "fname":"item3.md", "specialImage":"/img/Item3_img.png"},
 {"id": "item4", "fname":"item4.md", "specialImage":"/img/Item4_img.png"},
-{"id": "backgroundheader", "specialImage":"/img/backgroundheader.jpg"},
 {"id": "item5", "fname":"item5.md", "specialImage":"/img/Item5_img.png"}];
