187afed891e10cde983d8a45f314d2c86422262a
Simrit Dhillon <simritdhillon@Simrits-MacBook-Pro.local>
2020-12-07 19:56:46 -0500

    trying to add header image

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
 export default [{"title":"About", "fname":"about.md", "specialImage":"/img/menagerie.png"},
-{"title":"Header Image", "specialImage":"/img/backgroundheader,jpg"},
 {"title":"index", "fname":"index.md"},
 {"title":"Contact", "fname":"contact.md"}];
