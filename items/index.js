063751016592441def1b9c80a8c2b8f9d9507930
Simrit Dhillon <simritdhillon@Simrits-MacBook-Pro.local>
2020-12-19 15:31:23 -0500

    adding alt text to img one

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/Item1_img.png"},
+export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/Item1_img.png", alt="mockup image of Mentolink"},
 {"id": "item2", "fname":"item2.md", "specialImage":"/img/Item2_img.png"},
 {"id": "item3", "fname":"item3.md", "specialImage":"/img/Item3_img.png"},
 {"id": "item4", "fname":"item4.md", "specialImage":"/img/Item4_img.png"},
